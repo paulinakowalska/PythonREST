2bfb68c22ad0dc916ecf1575cbfe678f153f6b6b
paulinakowalska <paula.kow@vp.pl>
2017-06-09 20:54:43 +0200

    test

--- client.py
+++ client.py
@@ -4,3 +4,5 @@ conn = http.client.HTTPConnection("10.129.2.148", 8081)
 conn.request("PUT", "C:\\Users\\paula\\Desktop\\client.py", BODY)
 response = conn.getresponse()
 print(response.status, response.reason)
+
+#test
