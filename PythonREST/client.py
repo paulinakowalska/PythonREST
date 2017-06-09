5601482127c0a2ef17cea7b3a7e584dfae5fea20
paulinakowalska <paula.kow@vp.pl>
2017-06-09 21:40:14 +0200

    client - read and send file to server

---
+++ PythonREST/client.py
@@ -0,0 +1,6 @@
+import http.client
+BODY = "***filecontents***"
+conn = http.client.HTTPConnection("10.129.2.148", 8081)
+conn.request("PUT", "C:\\Users\\paula\\Desktop\\client.py", BODY)
+response = conn.getresponse()
+print(response.status, response.reason)
