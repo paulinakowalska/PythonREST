2b93b054c0b5eaf6e45c3aa0e4882bbf6a01c7d6
Ayolek <ayolek@gmail.com>
2017-06-10 16:10:49 +0200

    simple response to client from server for client's GET request

--- PythonREST/server.py
+++ PythonREST/server.py
@@ -1,5 +1,29 @@
+import socketserver
+
 import PythonREST.executor
+import http.server
 
 
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("got your message. thank you!".encode())
+
+IP = "192.168.0.3"
+PORT = 8081
+
+Handler = MyHandler
+
+with socketserver.TCPServer((IP, PORT), Handler) as httpd:
+    print("server started.")
+    httpd.serve_forever()
+
+# TODO fix unreachable code below
 file_path = "D:\\Hello.py"
 PythonREST.executor.execute_file(file_path)
+
