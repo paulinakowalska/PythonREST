38a444c5be32ba4cd82f9b7b1bd1f251ff3094f0
Ayolek <ayolek@gmail.com>
2017-06-10 17:56:35 +0200

    added loading content and sending content methods

--- PythonREST/client.py
+++ PythonREST/client.py
@@ -1,6 +1,32 @@
 import http.client
-BODY = "***filecontents***"
-conn = http.client.HTTPConnection("192.168.0.3", 8081)
-conn.request("PUT", "/", BODY)
-response = conn.getresponse()
+
+
+server_ip = "192.168.0.3"
+PORT = 8081
+
+
+def load_file(file_path):
+    """
+    loads file content from given path"
+    :param file_path:
+    :return: file content
+    """
+    file = open(file_path)
+    return file.read()
+
+
+def send_file(content):
+    """
+    sends content to server defined in client module
+    :param content:
+    :return: server response
+    """
+    conn = http.client.HTTPConnection(server_ip, PORT)
+    conn.request("PUT", "/", content)
+    return conn.getresponse()
+
+file_path = "D:\\Hello.py"
+content = load_file(file_path)
+response = send_file(content)
+
 print(response.status, response.reason, response.read())
