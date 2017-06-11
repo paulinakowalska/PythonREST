a6e12af48c26cf6d61a5043e3bcd61155f427b23
Ayolek <ayolek@gmail.com>
2017-06-11 14:19:18 +0200

    fixed send_file method and added run_client method to imitate clients behavior

--- PythonREST/client.py
+++ PythonREST/client.py
@@ -1,8 +1,9 @@
 import http.client
+import time
 
-
-server_ip = "192.168.0.3"
-PORT = 8081
+IP = "192.168.0.3"
+PORT = 8080
+file_path = "D:\\Hello.py"
 
 
 def load_file(file_path):
@@ -15,18 +16,31 @@ def load_file(file_path):
     return file.read()
 
 
-def send_file(content):
+def send_file(name, content, server_ip, PORT):
     """
     sends content to server defined in client module
-    :param content:
+    :param:
+        name: name of file to send
+        content: content of file to send
+        server_ip: ip of server where content should be sent
+        PORT: port where server is listening for requests
     :return: server response
     """
     conn = http.client.HTTPConnection(server_ip, PORT)
-    conn.request("PUT", "/", content)
+    conn.request("PUT", "/".__add__(name), content)
     return conn.getresponse()
 
-file_path = "D:\\Hello.py"
-content = load_file(file_path)
-response = send_file(content)
 
-print(response.status, response.reason, response.read())
+def run_client():
+    """
+    fakes client behavior with following steps:
+    1. loads file from built-in filepath
+    2. sends content of loaded file to server defined by built-in IP and PORT variables
+    :return: None - prints received responses
+    """
+    time.sleep(2)
+    print("loading file...")
+    content = load_file(file_path)
+    print("file loaded. sending file...")
+    response = send_file("program1", content, IP, PORT)
+    print(response.status, response.reason, response.read().decode())
