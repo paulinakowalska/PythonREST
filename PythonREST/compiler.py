091831ddc553d79204ab40c0b60c55e43fecb188
Ayolek <ayolek@gmail.com>
2017-06-11 14:17:23 +0200

    fixed compilation method now takes content as argument instead of path to file

--- PythonREST/compiler.py
+++ PythonREST/compiler.py
@@ -1,23 +1,16 @@
-import os
 import py_compile
 
 
-def compile_file(file_path):
+def compile_file(content):
     """
-    :param file_path:
+    :param content:
     :return:
         compilation success: 1
         compilation error: 0
     """
-    if file_path and os.path.isfile(file_path):
-        print("compiling...")
-        try:
-            py_compile.compile(file_path)
-            print("compilation successful")
-            return 1
-        except Exception as e:
-            print("this file can not be compiled:", e.args)
-            return 0
-    else:
-        print("file doesn't exist or is not a file")
+    try:
+        py_compile.compile(content)
+        return 1
+    except Exception as e:
+        print("this file can not be compiled:", e.args)
         return 0
