ebc3204759c1b32aabcf358cea45fbd76d4a1676
Ayolek <ayolek@gmail.com>
2017-06-11 14:14:51 +0200

    fixed execution method. removed get_result method

--- PythonREST/executor.py
+++ PythonREST/executor.py
@@ -1,38 +1,11 @@
-import os
-import PythonREST.compiler
-
-
-result = dict()
-
-
-def execute_file(file_path):
+def execute_program(content):
     """
-    executes file from given path and saves output.
-    use get_result() to receive program output
-    :param file_path
-    :return:
-        file executed: 1
-        error occurred: 0
+    executes given content of program.
+    :param content: content of program to execute
+    :return: output of executed program
     """
-    if file_path and os.path.isfile(file_path):
-        if PythonREST.compiler.compile_file(file_path):
-            file = open(file_path)
-            print("executing file...")
-            exec(file.read(), dict(), result)
-            print("file executed. use get_result() to get program output")
-            file.close()
-            return 1
-        else:
-            print("couldn't compile file")
-            return 0
-    else:
-        print("file doesn't exist or is unable to open")
-        return 0
-
-
-def get_result():
-    """
-    gets output from executed file
-    :return: program output
-    """
+    result = dict()
+    print("executing file...")
+    exec(content, dict(), result)
+    print("file executed.")
     return result["result"]
