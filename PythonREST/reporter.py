c62fb347b3927c5dc7d92f413efe78d75979a2d2
Wojciech <wk@com.pl>
2017-06-12 02:01:26 +0200

    refractored reporter for better testing experience

--- PythonREST/reporter.py
+++ PythonREST/reporter.py
@@ -1,15 +1,35 @@
 import os
 
+class Reporter:
 
-def save_program(content):
-    """
-    creates new file with a given content at server's path containing programs
-    :param content: programs code
-    :return: None
-    """
-    path = "/server/programs"
-    if not os.path.exists(path):
-        os.makedirs(path)
-    files = len(os.listdir(path))
-    file = open(path.__add__("/program".__add__(str(files+1)).__add__(".py")), 'w')
-    file.write(content.decode())
+    def save_program(self, content):
+        """
+        creates new file with a given content at server's path containing programs
+        :param content: programs code
+        :return: None
+        """
+
+        path = "/server/programs"
+        
+        self.create_file_directory_if_not_exists(path)
+
+        files = len(os.listdir(path))
+        file = open(path.__add__("/program".__add__(str(files+1)).__add__(".py")), 'w')
+        file.write(content.decode())
+
+    def get_file_path(content):
+
+        path = "/server/programs"
+
+        return path
+
+    def create_file_directory_if_not_exists(self, path):
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    def is_directory_exists(self, path):
+
+        return os.path.exists(path)
+
+
+
