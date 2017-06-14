7aab3725564afeaf40bdcbe5de0e64b360bdc4d0
Ayolek <ayolek@gmail.com>
2017-06-14 17:19:37 +0200

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-import http.server
-
-BODY = "***filecontents***"
-conn = http.client.HTTPConnection("10.129.2.148", 8081)
-conn.request("PUT", "C:\\Users\\paula\\Desktop\\client.py", BODY)
-response = conn.getresponse()
-print(response.status, response.reason)
-
-#test
