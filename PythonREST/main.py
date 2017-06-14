d285cd47b314e463ef2d8533094a6be9c65aae61
Ayolek <ayolek@gmail.com>
2017-06-14 22:51:06 +0200

    imitation of multiple clients requesting server

--- PythonREST/main.py
+++ PythonREST/main.py
@@ -4,8 +4,10 @@ import threading
 
 class Main:
 
-    IP = "192.168.43.92"
+    # IP = "192.168.43.92"
     # IP = "10.129.4.227"
+    IP = "192.168.0.3"
+
     PORT = 8080
     k = client.Client
     s = server.Server
@@ -15,14 +17,17 @@ class Main:
 
     def start(self):
         print("starting program")
-        client = threading.Thread(target=self.k.run_client, args=(self,), name="client")
+        client = threading.Thread(target=self.k.run_client, args=(self, 1), name="client")
         server = threading.Thread(target=self.s.run, args=(self, self.IP, self.PORT), name="server")
+        client2 = threading.Thread(target=self.k.run_client, args=(self, 2), name="client2")
 
         client.start()
         server.start()
+        client2.start()
 
         server.join()
         client.join()
+        client2.join()
 
 mk = Main()
 mk.start()
