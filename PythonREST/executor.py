b24922a0d4c9c61d61c41e2ce5e33aa08bbf2612
Ayolek <ayolek@gmail.com>
2017-06-15 09:20:02 +0200

    fixed executor now tries to execute program and returns try result and then result of program or exception info

--- PythonREST/executor.py
+++ PythonREST/executor.py
@@ -6,6 +6,10 @@ def execute_program(content):
     """
     result = dict()
     print("executing file...")
-    exec(content, dict(), result)
-    print("file executed.")
-    return result["result"]
+    try:
+        exec(content, dict(), result)
+        print("file executed.")
+        return 1, result["result"]
+    except Exception as e:
+        return 0, str(e.args)
+
