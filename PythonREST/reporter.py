9f68aed993254ddff256b72f7055f4e8efc5c1a1
Ayolek <ayolek@gmail.com>
2017-06-15 11:52:22 +0200

    not working comparison

--- PythonREST/reporter.py
+++ PythonREST/reporter.py
@@ -1,10 +1,12 @@
 import os
 from builtins import staticmethod
+import filecmp
+from fnmatch import fnmatch
 
 
 class Reporter:
 
-    path = "/server/programs"
+    path = "/server/programs/"
 
     def save_program(self, content):
         """
@@ -12,11 +14,23 @@ class Reporter:
         :param content: programs code
         :return: None
         """
-        self.create_file_directory_if_not_exists(self.path)
+        self.create_file_directory_if_not_exists(self, self.path)
 
         files = len(os.listdir(self.path))
-        file = open(self.path.__add__("/program".__add__(str(files+1)).__add__(".py")), 'w')
+        name = "program".__add__(str(files+1).__add__(".py"))
+        file = open(self.path.__add__(name), 'w')
         file.write(content.decode())
+        self.compare(name)
+
+    def compare(self, name):
+        for i in (os.listdir(self.path)):
+            if i != name:
+                # comparision = open(self.path.__add__(i)).read() == open(self.path.__add__(name)).read()
+                # comparision = fnmatch(open(self.path.__add__(i)).read(), open(self.path.__add__(name)).read())
+                print("comparing {} and {} result: ".format(i, name), comparision)
+
+
+
 
     @staticmethod
     def get_file_path(self):
@@ -27,5 +41,6 @@ class Reporter:
         if not os.path.exists(path):
             os.makedirs(path)
 
+    @staticmethod
     def does_directory_exist(self, path):
         return os.path.exists(path)
